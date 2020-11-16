776ce932ffd51dc3656c8fdbe963e2bdae680322
thoffman1 <tylerhoff1@gmail.com>
2020-11-16 11:24:34 -0500

    hw5

--- hws/hw5/code/diffusion_map.py
+++ hws/hw5/code/diffusion_map.py
@@ -1,10 +1,10 @@
 import numpy as np
 from numpy.linalg import eig, pinv
-from scipy.spatial.distance import pdist
+from scipy.spatial.distance import pdist, squareform
 
 def diffusion_map(X, epsilon=0.01, t=50, alpha=0, dim=2):
     # Define diffusion kernel
-    K = np.exp(-pdist(X)/epsilon)
+    K = np.exp(-squareform(pdist(X))/epsilon)
     
     # Renormalize
     q = K.sum(axis=1)
@@ -17,12 +17,12 @@ def diffusion_map(X, epsilon=0.01, t=50, alpha=0, dim=2):
     P = pinv(Da) @ Ka
 
     # Compute eigendecomposition and embedding
-    R, Lam = eig(P)
+    Lam, R = eig(P)
     if dim == 2:
         return np.array([Lam[0]**((1-alpha)*t) * R[0,:], 
-                         Lam[1]**((1-alpha)*t) * R[1,:]])
-    elif dim == 3
+                         Lam[1]**((1-alpha)*t) * R[1,:]]).T
+    elif dim == 3:
         return np.array([Lam[0]**((1-alpha)*t) * R[0,:], 
                          Lam[1]**((1-alpha)*t) * R[1,:],
-                         Lam[2]**((1-alpha)*t) * R[2,:]])
+                         Lam[2]**((1-alpha)*t) * R[2,:]]).T
     else: raise RuntimeError(f'dim must be 2 or 3 (was {dim}')
